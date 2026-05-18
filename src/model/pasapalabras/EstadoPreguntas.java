5f9892118bf0af524906170b46f3dcb4d141ba74
Jose Antonio De la Mora <jadelamoraloyola@gmail.com>
2026-05-18 12:22:51 +0200

    Delete src directory

--- src/model/pasapalabras/EstadoPreguntas.java
+++
@@ -1,8 +0,0 @@
-package model.pasapalabras;
-
-public enum EstadoPreguntas {
-    PENDIENTE, // Color azul (no se ha respondido)
-    ACERTADA,  // Color verde
-    FALLADA,   // Color rojo
-    PASADA     // Se mantiene azul/amarillo (para volver a ella luego)
-}
